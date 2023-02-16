c888e3805248c90a173922c964debe5098d37c29
Josh Faigan <djjoker13@gmail.com>
2023-02-16 13:41:06 -0500

    styled

--- src/components/calc.js
+++ src/components/calc.js
@@ -7,14 +7,16 @@ function Calculator() {
   const [tip, setTip] = useState(0);
   const [people, setPeople] = useState(0);
   const [total, setTotal] = useState(0);
+  const [tipTotal, setTipTotal] = useState(0);
+  const [totalPerPerson, setTotalPerPerson] = useState(0);
   
   function calculateTotal(bill, tip, people) {
+    setTipTotal((bill * tip) / 100);
     tip = (tip / 100) + 1;
     let total = ((bill * tip) / people).toFixed(2);
-    setTotal(total);
-    if (total > 0) {
-      return <h3>${total}</h3>
-    }
+    
+    setTotal(total)
+    setTotalPerPerson(total / people);
   }
 
   return (
@@ -39,6 +41,10 @@ function Calculator() {
             <button>Calculate</button>
           </div>
         <div className="Results">
+          <h2>Tip Amount</h2>
+          {tipTotal > 0 ? <h3>${tipTotal}</h3> : ""}
+          <h2>Total Per Person</h2>
+          {totalPerPerson > 0 ? <h3>${totalPerPerson}</h3> : ""}
           <h2>Total Amount</h2>
           {total >0 ? <h3>${total}</h3> : ""}
         </div>
